c5900c590e6f16f58a98210c2e7adb3d942fa0fe
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-03-30 14:14:31 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,8 +7,14 @@ BASE_URL = "https://webapp-ocr-p7-b2d0cuafdhgyf8fx.canadacentral-01.azurewebsite
 async def test_negative_tweet_prediction():
     tweet = "I hate my family"
    
-    async with httpx.AsyncClient() as client:
-        response = await client.post(f"{BASE_URL}/predict", params={"tweet": tweet})
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        try:
+            response = await client.post(f"{BASE_URL}/predict", params={"tweet": tweet})
+            print(f"Status code : {response.status_code}")
+            print(f"Response : {response.json}")
+        except Exception as e:
+            print(f"Request failed: {e}")
+            assert False
        
     assert response.status_code == 200
     data = response.json()
@@ -19,8 +25,14 @@ async def test_negative_tweet_prediction():
 async def test_positive_tweet_prediction():
     tweet = "I love my family"
    
-    async with httpx.AsyncClient() as client:
-        response = await client.post(f"{BASE_URL}/predict", params={"tweet": tweet})
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        try:
+            response = await client.post(f"{BASE_URL}/predict", params={"tweet": tweet})
+            print(f"Status code : {response.status_code}")
+            print(f"Response : {response.json}")
+        except Exception as e:
+            print(f"Request failed: {e}")
+            assert False
        
     assert response.status_code == 200
     data = response.json()
